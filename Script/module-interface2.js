64ae0bb563f4d6db3aac8173872e058e36902ffc
elmanuchuky <elmanu.chuky@gmail.com>
2017-04-11 16:20:21 -0300

    Funcional el filtro bascio de ingred disponibles
    Tambien se hizo escalable la feature para cantidad maxima de ingredientes extras permitidos

--- Script/module-interface2.js
+++ Script/module-interface2.js
@@ -7,6 +7,7 @@ angular
         cInterface2.object = $firebaseObject(ref);
         cInterface2.allRecipes = new Object();
         cInterface2.allRecipesAux = new Object();
+        cInterface2.extraIngredientsAllowed = 1;
 
         // Traer todas las recetas
         cInterface2.bringEveryRecipe = function(){
@@ -18,45 +19,39 @@ angular
         // Traer solo las recetas que esten coincidentes con el filtro
         cInterface2.bringSomeRecipe = function(){
             for (ingredient in JSON.parse(localStorage.getItem("myData"))){
-                db.ref('db/ingredients/' + ingredient + '/recipes').once('value', function(snapshot){
-                    alert(ingredient);
+                db.ref('db/ingredients/' + ingredient + '/recipes').once('value').then(function(snapshot){
                     for (key in snapshot.val()){
-                        db.ref('db/recipes').child(key).once('value', function(snapshot){
-                            cInterface2.allRecipes[key] = snapshot.val();
+                        db.ref('db/recipes/').child(key).once('value', function(snap){
+                            cInterface2.allRecipesAux[key] = snap.val().ingredients;
                         })
+                    }
+                    for (recipe in cInterface2.allRecipesAux){
+                        var extraIngredients = 0;
+                        for (ingredients in cInterface2.allRecipesAux[recipe]){
+                            if (!(localStorage.getItem("myData")).includes(ingredients)){
+                                extraIngredients++;
+                            }
+                        }
+                        if (extraIngredients <= cInterface2.extraIngredientsAllowed){
+                            cInterface2.allRecipes[recipe] = true;
+                            db.ref('db/recipes/').child(recipe).once('value', function(snapshot){
+                                cInterface2.allRecipes[recipe] = snapshot.val();
+                                
+                            })
+                        }
                     }
                 })
             }
         }
 
-/*
-        db.ref('db/recipes').once('value').then(function(snapshot){
-            for ()
-            cInterface2.allRecipes[Object.keys(snapshot.val())] = {
-                ingredients
-            }
-            console.log(snapshot.val());
-        })
-*/
-
-        // Funcion para probar cosas con el boton
-        cInterface2.testApp = function(){
-            for (recipe in cInterface2.allRecipesAux){
-                db.ref('db/recipes/' + recipe).once('value', function(snapshot){
-                    cInterface2.allRecipes[recipe] = snapshot.val();
-                })
-            }
-            console.log(JSON.parse(localStorage.getItem("myData")));
+        // Envia al localStorage la receta seleccionada
+        cInterface2.selectRecipe = function(index){
+            localStorage.setItem("selectedRecipe", Object.keys(cInterface2.allRecipes)[index])
         }
 
         if (_.isEmpty(JSON.parse(localStorage.getItem("myData")))){
             cInterface2.bringEveryRecipe();
         }else{
             cInterface2.bringSomeRecipe();
-        }
-
-        // Envia al localStorage la receta seleccionada
-        cInterface2.selectRecipe = function(index){
-            localStorage.setItem("selectedRecipe", Object.keys(cInterface2.allRecipes)[index])
         }
     })
