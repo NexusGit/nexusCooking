766474197437e7206eb340fe29fa3823a09ddfea
elmanuchuky <elmanu.chuky@gmail.com>
2017-05-23 17:05:58 -0300

    Add files via upload

--- res/js/menu-filtros.js
+++ res/js/menu-filtros.js
@@ -1,31 +1,31 @@
+myArrowFilter = function(){
+    if ($("#rotateIco").hasClass("derotateIco")){
+        $("#rotateIco").toggleClass("rotateIco");
+    }else{
+        $("#rotateIco").toggleClass("derotateIco");
+    }
+}
 
 cats=function(){
-
     $("#cats").slideToggle();
     $("#dif").slideUp();
     $("#tim").slideUp();
-
     $( "#cats-activo" ).toggleClass( "activo" )
     $( "#dif-activo" ).removeClass( "activo" );
     $( "#tim-activo" ).removeClass( "activo" );
-
 }
 dif=function(){
-
     $("#dif").slideToggle();
     $("#cats").slideUp();
     $("#tim").slideUp();
-
     $( "#dif-activo" ).toggleClass( "activo" )
     $( "#cats-activo" ).removeClass( "activo" );
     $( "#tim-activo" ).removeClass( "activo" );
 }
 tim=function(){
-
     $("#tim").slideToggle();
     $("#cats").slideUp();
     $("#dif").slideUp();
-
     $( "#tim-activo" ).toggleClass( "activo" )
     $( "#cats-activo" ).removeClass( "activo" );
     $( "#dif-activo" ).removeClass( "activo" );
